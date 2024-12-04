77c141a1a04639f3a4d0ef3840e9804384eb9e38
Quallen <alexander.holland@ctunitedway.org>
2024-12-04 16:29:10 -0500

    count the number of XMAS

---
+++ 2024/day_4/problem_4.rb
@@ -0,0 +1,116 @@
+require 'pry-byebug'
+
+class Search
+  attr_accessor :input, :length
+
+  def initialize
+    @input = File.read('problem_4_input.txt').lines.map(&:chomp)
+    @length = 140
+    @grid = Array.new(length){Array.new(length)}
+    init_grid
+    @possible_xmas_starts = []
+    get_start_locations
+    @xmas_count = 0
+  end
+
+  def init_grid
+    input.each_with_index do |line, row_index|
+      line.each_char.with_index  do |character, column_index|
+        @grid[row_index][column_index] = Location.new(letter: character, coords: [row_index, column_index] )
+      end
+    end
+  end
+
+  def get_start_locations
+    input.each_with_index do |line, row_index|
+      line.each_char.with_index  do |character, column_index|
+        @possible_xmas_starts << @grid[row_index][column_index] if @grid[row_index][column_index].word_start?
+      end
+    end
+  end
+
+  def get_xmas_count
+    @possible_xmas_starts.each do |location|
+      %w(right left down up diagonal_right_down diagonal_right_up diagonal_left_down diagonal_left_up).each do |direction|
+        send("scan_#{direction}", location)
+      end
+    end
+    puts @xmas_count
+  end
+
+
+  def scan_right(point)
+    return if point.x + 3 > length - 1
+    @xmas_count += 1 if @grid[point.x+1][point.y].m? && @grid[point.x+2][point.y].a? && @grid[point.x+3][point.y].s?
+  end
+
+  def scan_left(point)
+    return if point.x - 3 < 0
+    @xmas_count += 1 if @grid[point.x-1][point.y].m? && @grid[point.x-2][point.y].a? && @grid[point.x-3][point.y].s?
+  end
+
+  def scan_down(point)
+    return if point.y + 3 > length - 1
+    @xmas_count += 1 if @grid[point.x][point.y+1].m? && @grid[point.x][point.y+2].a? && @grid[point.x][point.y+3].s?
+  end
+
+  def scan_up(point)
+    return if point.y - 3 < 0
+    @xmas_count += 1 if @grid[point.x][point.y-1].m? && @grid[point.x][point.y-2].a? && @grid[point.x][point.y-3].s?
+  end
+
+  def scan_diagonal_right_down(point)
+    return if point.x + 3 > length - 1 || point.y + 3 > length - 1
+    @xmas_count += 1 if @grid[point.x+1][point.y+1].m? && @grid[point.x+2][point.y+2].a? && @grid[point.x+3][point.y+3].s?
+  end
+
+  def scan_diagonal_right_up(point)
+    return if point.x + 3 > length - 1 || point.y - 3 < 0
+    @xmas_count += 1 if @grid[point.x+1][point.y-1].m? && @grid[point.x+2][point.y-2].a? && @grid[point.x+3][point.y-3].s?
+  end
+
+  def scan_diagonal_left_down(point)
+    return if point.x - 3 < 0 || point.y + 3 > length - 1
+    @xmas_count += 1 if @grid[point.x-1][point.y+1].m? && @grid[point.x-2][point.y+2].a? && @grid[point.x-3][point.y+3].s?
+  end
+
+  def scan_diagonal_left_up(point)
+    return if point.x - 3 < 0 || point.y - 3 < 0
+    @xmas_count += 1 if @grid[point.x-1][point.y-1].m? && @grid[point.x-2][point.y-2].a? && @grid[point.x-3][point.y-3].s?
+  end
+
+  class Location
+    attr_accessor :coordinates, :letter
+    def initialize(letter: ,coords:)
+      @letter = letter
+      @coordinates = coords
+    end
+
+    def x
+      coordinates.first
+    end
+
+    def y
+      coordinates.last
+    end
+
+    def word_start?
+      letter == 'X'
+    end
+
+    def m?
+      letter == 'M'
+    end
+
+    def a?
+      letter == 'A'
+    end
+
+    def s?
+      letter == 'S'
+    end
+
+  end
+end
+search = Search.new
+search.get_xmas_count
